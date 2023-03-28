8cd5d126ba723183cfe7802e4c9b9a8891839940
Andrew Alsing <andrew32alsing@gmail.com>
2023-03-28 15:14:13 -0700

    completed all challenges

---
+++ Lab1.playground/Pages/Constant or Variable?.xcplaygroundpage/Contents.swift
@@ -0,0 +1,32 @@
+/*:
+ ## App Exercise - Fitness Tracker: Constant or Variable?
+ 
+ >These exercises reinforce Swift concepts in the context of a fitness tracking app.
+ 
+ There are all sorts of things that a fitness tracking app needs to keep track of in order to display the right information to the user. Similar to the last exercise, declare either a constant or a variable for each of the following items, and assign each a sensible value. Be sure to use proper naming conventions.
+ 
+ - Name: The user's name
+ - Age: The user's age
+ - Number of steps taken today: The number of steps that a user has taken today
+ - Goal number of steps: The user's goal for number of steps to take each day
+ - Average heart rate: The user's average heart rate over the last 24 hours
+ */
+let name = "Andrew"
+print("Constant: because I'm not going to change my name")
+
+var age = 24
+print("Variable: my age increases by 1 every year")
+
+var stepsTaken = 5000
+print("Variable: steps taken are increasing")
+
+let stepsGoal = 10000
+print("Constant: steps goal doesn't need to change for now")
+
+var averageHeartRate = 65
+print("Variable: my average heart rate changes")
+
+/*:
+ Now go back and add a line after each constant or variable declaration. On those lines, print a statement explaining why you chose to declare the piece of information as a constant or variable.
+ */
+//: [Previous](@previous)    [Next](@next)
